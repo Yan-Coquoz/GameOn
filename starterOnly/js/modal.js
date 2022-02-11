8c421b6a8527c2fd661a1f95beeb650a7fe0d287
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-02-11 16:17:46 +0100

    test de formulaire

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -10,13 +10,23 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeBtn = document.body.querySelector(".close");
+const closeBtn = document.querySelector(".close");
+const formulaire = document.querySelector("form");
+const submitBtn = document.querySelector(".btn-submit");
+const prenom = document.querySelector("#first");
+const nom = document.querySelector("#last");
+const email = document.querySelector("#email");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 closeBtn.addEventListener("click", closeModal);
+
+// écoute du formulaire
+formulaire.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  myForm();
+});
 
 // launch modal form
 function launchModal() {
@@ -25,4 +35,9 @@ function launchModal() {
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
+function myForm() {
+  console.log(`nom : ${nom.value}
+prenom : ${prenom.value}
+email : ${email.value}`);
 }
