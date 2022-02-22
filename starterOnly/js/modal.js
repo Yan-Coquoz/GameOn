574adb8888a7075a7331b78cdf1eeb2a761a76d8
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-02-22 11:22:47 +0100

    accueil en responsive ok

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,9 +1,9 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var topNav = document.getElementById("myTopnav");
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
   } else {
-    x.className = "topnav";
+    topNav.className = "topnav";
   }
 }
 
