c84928cc83d720c609d05e20b7d6e20390d066bb
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-03-07 09:14:46 +0100

    SoC de la validation

--- src/index.js
+++ src/index.js
@@ -279,33 +279,3 @@ function checkFormValidation() {
     validationMessage();
   }
 }
-
-function validationMessage() {
-  // creation d'element du DOM :
-  // balise <p>
-  const message = document.createElement("p");
-  // balise <button>
-  const btn = document.createElement("button");
-  // creation / placement du texte de confirmation
-  message.innerHTML = "Merci !<br> Votre réservation a été reçue.";
-  message.style.textAlign = "center";
-  // ajout d'une classe
-  message.classList.add("message");
-  // ajout du texte du bouton et d'une classe
-  btn.textContent = "Fermer";
-  btn.classList.add("button");
-  // style de la modale
-  modalContent.style.display = "flex";
-  modalContent.style.flexDirection = "column";
-  modalContent.style.justifyContent = "space-between";
-  modalContent.style.alignItems = "center";
-  modalContent.style.height = "80vh";
-  // ajout de style sur le bouton
-  btn.style.marginBottom = "1rem";
-  btn.style.padding = "1rem 4rem";
-  // placement des elements
-  modalContent.appendChild(message);
-  modalContent.appendChild(btn);
-  // écoute du bouton
-  btn.addEventListener("click", closeModal);
-}
